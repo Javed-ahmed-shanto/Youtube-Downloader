29d5f694c9095f8f57181ed60b2d1c3935d5f424
shanto <shantoa729@gmail.com>
2023-06-08 15:53:08 +0900

    updated

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pytube import YouTube
 from sys import argv
 
-link = argv[1]
+link = argv[1] # give your link in the terminal after your file name within a quote
 yt = YouTube(link)
 
 print("Title: ", yt.title)
